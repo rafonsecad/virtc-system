24604eadfffd77e37675b2b776c54436d1b779a8
L3v1t4s1um <83776248+l3v1t4s1um@users.noreply.github.com>
2021-11-11 01:38:15 -0500

    [Update] Prepared to be undefined

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -4,23 +4,21 @@ import { errorsCodes } from '../Constants/errors_codes';
  * Create a new error according to the code.
  * @class
  * @param {number} code Error code
- * @param {boolean} s void (false) | string (true)
- * @returns {(void|string)} soid with the error | string with the error
+ * @param {(boolean|undefined)} s void (false) | string (true) | void (undefined)
+ * @returns {(void|string)} void with the error | string with the error
  */
 
 class Err {
     code: number;
-    s: boolean;
+    s: boolean | undefined;
 
-    constructor(code: number, s: boolean) {
+    constructor(code: number, s: boolean | undefined) {
         this.code = code;
         this.s = s;
     }
 
     getError(): void | string {
-        if (!this.s) this.s = false;
-
-        if (this.s === false) {
+        if (this.s === undefined || this.s === false) {
             console.error(errorsCodes[this.code].content);
         } else {
             return errorsCodes[this.code].content;
