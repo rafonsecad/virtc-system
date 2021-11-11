266020dbf4d4f067749e1c9fe41f6782633193a6
L3v1t4s1um <83776248+l3v1t4s1um@users.noreply.github.com>
2021-11-11 01:24:17 -0500

    [Update] prettier format

--- src/Constants/errors_codes.ts
+++ src/Constants/errors_codes.ts
@@ -1,6 +1,7 @@
 export const errorsCodes: object | any = {
     423: {
         code: 423,
-        content: 'Error (423)\n    An error occurred while trying to connect to the database.'
-    }
-}
+        content:
+            'Error (423)\n    An error occurred while trying to connect to the database.',
+    },
+};
